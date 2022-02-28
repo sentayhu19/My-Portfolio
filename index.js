64e8b80f1df99a795e277c3fac7a2a499aae30c3
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-02-28 17:44:27 +0300

    event to when the hamburger is clicked Events where added

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamList = document.querySelector("#ham_menu");
+const section = document.querySelectorAll("#n1");
+const close = document.querySelector("#coverItem");
+hamList.addEventListener("click", () => {
+  const ham = document.getElementById("ham_menu");
+  document.getElementById("cover").style.cssText =
+    " width: 100%; height: 100%; ";
+  document.getElementById("coverItem").style.cssText = "display: block";
+  document.getElementById("menu").style.cssText = "display: flex; z-index: 2;";
+});
+section.forEach((e) => {
+  e.addEventListener("click", function () {
+    document.getElementById("cover").style.cssText = " width: 0%; height: 0%; ";
+  });
+});
