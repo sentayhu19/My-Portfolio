78437f9221bbe68569ad29d1b9e89fcf4bb641e5
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-03-01 12:45:11 +0300

    eslintrc issue fixed

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const hamList = document.querySelector("#ham_menu");
-const section = document.querySelectorAll(".n2");
-const close = document.querySelector("#coverItem");
-hamList.addEventListener("click", function () {
-  document.getElementById("cover").style.cssText =
-    " width: 100%; height: 100%; ";
-  document.getElementById("coverItem").style.cssText = "display: block";
-  document.getElementById("menu").style.cssText = "display: flex; z-index: 2;";
-  document.getElementById("body").style.cssText = "overflow: hidden;";
+const hamList = document.querySelector('#ham_menu');
+const section = document.querySelectorAll('.n2');
+const close = document.querySelector('#coverItem');
+hamList.addEventListener('click', () => {
+  document.getElementById('cover').style.cssText = ' width: 100%; height: 100%; ';
+  document.getElementById('coverItem').style.cssText = 'display: block';
+  document.getElementById('menu').style.cssText = 'display: flex; z-index: 2;';
+  document.getElementById('body').style.cssText = 'overflow: hidden;';
 });
 section.forEach((e) => {
-  e.addEventListener("click", () => {
-    document.getElementById("cover").style.cssText = " width: 0%; height: 0%; ";
-    document.getElementById("body").style.cssText = "overflow: none;";
+  e.addEventListener('click', () => {
+    document.getElementById('cover').style.cssText = ' width: 0%; height: 0%; ';
+    document.getElementById('body').style.cssText = 'overflow: none;';
   });
 });
-close.addEventListener("click", function () {
-  document.getElementById("cover").style.cssText = " width: 0%; height: 0%; ";
-  document.getElementById("body").style.cssText = "overflow: none;";
+close.addEventListener('click', () => {
+  document.getElementById('cover').style.cssText = ' width: 0%; height: 0%; ';
+  document.getElementById('body').style.cssText = 'overflow: none;';
 });
